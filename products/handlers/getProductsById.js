58b53535374464ae363e9b853cbd0d550c20d3e7
Korbut Yura <korbuty@mail.ru>
2021-09-07 23:52:12 +0300

    feat: integrate RDS database

--- products/handlers/getProductsById.js
+++ products/handlers/getProductsById.js
@@ -1,25 +1,39 @@
 'use strict';
-const products = require('../mocks/products.json')
+const {Client} = require('pg')
 const {ErrorResponse} = require('../errors/ErrorResponse')
 
+const {PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD} = process.env
+
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000
+}
+
 module.exports.getProductsById = async (event, context) => {
+  const client = new Client(dbOptions);
+  await client.connect()
+
     try { 
-        const {productId} = event.pathParameters
-        const currentProduct = products.find(({id})=> id === productId)
-        console.log('currentProduct',currentProduct, productId );
-        if (!currentProduct) {
-            throw new ErrorResponse(`There is no products with id ${productId}`, 404)
-        }
-          return {
-            statusCode: 200,
-            body: JSON.stringify(
-              {
-                product: currentProduct,
-              },
-              null,
-              2
-            ),
-          };
+        const { productId } = event.pathParameters
+        const { rows } = await client.query(`
+          SELECT id, title, description, price, photoUrl, count 
+          FROM products
+          JOIN stocks ON products.id = stocks.product_id
+          WHERE products.id = '${productId}'
+      `).catch(()=>{
+        throw new ErrorResponse(`There is no products with id ${productId}`, 404)
+      })
+      return {
+        statusCode: 200,
+        body: JSON.stringify( rows, null, 2 ),
+      };
     } catch (_err) {
         let err = _err
         if (!(err instanceof ErrorResponse) ){
@@ -28,6 +42,8 @@ module.exports.getProductsById = async (event, context) => {
         }
        
         return err
+    } finally {
+      client.end()
     }
 
 };
--- products/handlers/getProductsList.js
+++ products/handlers/getProductsList.js
@@ -1,18 +1,40 @@
 'use strict';
-const products = require('../mocks/products.json')
+const {Client} = require('pg')
+const {ErrorResponse} = require('../errors/ErrorResponse')
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD} = process.env
+
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000
+}
 
 module.exports.getProductsList = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-        "Access-Control-Allow-Origin": "*" 
-    },
-    body: JSON.stringify(
-      {
-        products: products,
-      },
-      null,
-      2
-    ),
-  };
+  const client = new Client(dbOptions);
+  await client.connect()
+
+  try{
+    const { rows } = await client.query(`
+      SELECT id, title, description, price, photoUrl, count 
+      FROM products
+      JOIN stocks ON products.id = stocks.product_id
+    `)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify( rows, null, 2 ),
+    };
+  } catch (err) {
+    console.error(err);
+    return  new ErrorResponse(err.message)
+  } finally {
+    client.end()
+  }
 };
